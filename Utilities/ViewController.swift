e1b94088c8edcf3c20a10bd915310df1f04ce174
Andres Ramirez <andres.ramirez@thecuriouspixel.com>
2020-05-25 16:18:13 +0100

    Initial Commit

---
+++ Utilities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Utilities
+//
+//  Created by Jose A Ramirez on 25/05/2020.
+//  Copyright © 2020 The Curious Pixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
