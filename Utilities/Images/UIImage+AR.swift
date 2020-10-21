3dfb1b1ead43512e8786a0d173b616b0126d5f6a
Andres Ramirez <andres.ramirez@thecuriouspixel.com>
2020-10-21 07:48:46 +0100

    Add finding image by name locally or in SF with tint

--- Utilities/Images/UIImage+AR.swift
+++ Utilities/Images/UIImage+AR.swift
@@ -19,4 +19,31 @@ extension UIImage {
             self.draw(in: CGRect(origin: .zero, size: size))
         }
     }
+    
+    static func image(named: String, withTintColor tint: UIColor?) -> UIImage? {
+        
+        if let fileImage = UIImage(named: named) {
+            if let tintColor = tint {
+                return fileImage.withTintColor(tintColor)
+            }
+            
+            return fileImage
+        }
+        
+        return nil
+    }
+    
+    static func sfImage(named: String, config: SymbolConfiguration, withTintColor tint: UIColor?) -> UIImage? {
+        
+        if let galleryGfx = UIImage(systemName: named, withConfiguration: config) {
+         
+            if let tint = tint {
+                return galleryGfx.withTintColor(tint, renderingMode: .alwaysOriginal)
+            }
+            
+            return galleryGfx
+        }
+        
+        return nil
+    }
 }
