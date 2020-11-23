dd97e9bb89d3eec0c55a9aaa08cd673544f0a10d
Andres Ramirez <andres.ramirez@thecuriouspixel.com>
2020-11-23 08:17:59 +0000

    Add animation and color to TextCapture
    - Done button for keyboard
    - Fade in animations
    - Pass main tint color around

--- Utilities/Text Capture/ARTextCapture.swift
+++ Utilities/Text Capture/ARTextCapture.swift
@@ -16,15 +16,24 @@ class ARTextCapture: UIViewController {
     
     static let fieldOffset: CGFloat = -100
     
-    func requestText(parentVC: UIViewController) {
+    func requestText(parentVC: UIViewController, withTintColor tintColor: UIColor = .white) {
         parentVC.addChild(self)
         parentVC.view.addSubview(self.view)
         self.didMove(toParent: parentVC)
+        setup(tintColor: tintColor)
+        
+        UIView.animate(withDuration: 0.4) {
+            self.fieldBackground.alpha = 1
+            self.textField.alpha = 1
+        }completion: { (success) in
+            UIView.animate(withDuration: 0.4) {
+                self.label.alpha = 1
+            }
+        }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,24 +56,51 @@ class ARTextCapture: UIViewController {
         updateLayout()
     }
     
-    func setup() {
+    private weak var fieldBackground: UIView!
+    private weak var textFieldConstraint: NSLayoutConstraint!
+    private weak var textField: UITextField!
+    private weak var label: UILabel!
+    
+}
+
+extension ARTextCapture: UITextFieldDelegate {
+    
+    // new name captured at this point
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+// MARK: - Private
+extension ARTextCapture {
+    
+    private func setup(tintColor: UIColor) {
         addVisualEffectView()
-        addTextField()
+        addTextField(tintColor: tintColor)
+        addTitle(tintColor: tintColor)
+        
+        fieldBackground.alpha = 0
+        textField.alpha = 0
+        label.alpha = 0
     }
     
-    func addTextField() {
+    private func addTextField(tintColor: UIColor) {
         
         let bknd = addTextFieldBackground()
         
         let textField = UITextField()
         textField.textAlignment = .center
-        textField.textColor = .lightText
+        textField.textColor = .white
+        textField.returnKeyType = .done
+        textField.tintColor = tintColor
+        
         textField.delegate = self
         
         view.addSubview(textField)
         
         let lightPlaceHolderText = NSAttributedString(string: "Enter a new name",
-                                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
         textField.attributedPlaceholder = lightPlaceHolderText
         
         textField.translatesAutoresizingMaskIntoConstraints = false
@@ -77,13 +113,13 @@ class ARTextCapture: UIViewController {
         self.textField = textField
     }
     
-    func addVisualEffectView() {
+    private func addVisualEffectView() {
         let newView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
         view.addSubview(newView)
         newView.constrain(toParent: self.view, atCorners: .all)
     }
     
-    func addTextFieldBackground() -> UIView {
+    private func addTextFieldBackground() -> UIView {
         
         let widthMultiplier: CGFloat = 0.8
         
@@ -102,46 +138,53 @@ class ARTextCapture: UIViewController {
         
         return textBnkd
     }
-
+    
+    private func addTitle(tintColor: UIColor) {
+        
+        let label = UILabel()
+        label.text = "Rename"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .thin)
+        label.textColor = tintColor
+        label.setCharacterSpacing(2)
+        
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.widthAnchor.constraint(equalTo: fieldBackground.widthAnchor).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        label.leadingAnchor.constraint(equalTo: fieldBackground.leadingAnchor).isActive = true
+        
+        label.bottomAnchor.constraint(equalTo: fieldBackground.topAnchor, constant: -10).isActive = true
+        
+        self.label = label
+    }
+    
     private func updateLayout() {
         fieldBackground?.round(corners: .allCorners, radius: 12)
     }
     
     @objc private func keyboardWillShowOrHide(notification: NSNotification) {
-            // Get required info out of the notification
-            if let userInfo = notification.userInfo, let endValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey], let durationValue = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey], let curveValue = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] {
+        // Get required info out of the notification
+        if let userInfo = notification.userInfo, let endValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey], let durationValue = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey], let curveValue = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] {
+            
+            if let fieldBackground = fieldBackground {
                 
-                if let fieldBackground = fieldBackground {
+                let endRect = view.convert((endValue as AnyObject).cgRectValue, from: view)
+                let keyboardOverlap = (fieldBackground.frame.maxY - endRect.origin.y) + 10 // 10 is offset
+                
+                if endRect.height > view.frame.height/3 { // make sure we don't get random small keyboard events
                     
-                    let endRect = view.convert((endValue as AnyObject).cgRectValue, from: view)
-                    let keyboardOverlap = (fieldBackground.frame.maxY - endRect.origin.y) + 10 // 10 is offset
+                    self.textFieldConstraint?.constant = (keyboardOverlap > 0) ? -keyboardOverlap + Self.fieldOffset : Self.fieldOffset
                     
-                    if endRect.height > view.frame.height/3 { // make sure we don't get random small keyboard events
-                       
-                        self.textFieldConstraint?.constant = (keyboardOverlap > 0) ? -keyboardOverlap + Self.fieldOffset : Self.fieldOffset
-                        
-                        let duration = (durationValue as AnyObject).doubleValue
-                        let options = UIView.AnimationOptions(rawValue: UInt((curveValue as AnyObject).integerValue << 16))
-                        
-                        UIView.animate(withDuration: duration!, delay: 0, options: options, animations: {
-                            self.view.layoutIfNeeded()
-                        }, completion: nil)
-                    }
+                    let duration = (durationValue as AnyObject).doubleValue
+                    let options = UIView.AnimationOptions(rawValue: UInt((curveValue as AnyObject).integerValue << 16))
+                    
+                    UIView.animate(withDuration: duration!, delay: 0, options: options, animations: {
+                        self.view.layoutIfNeeded()
+                    }, completion: nil)
                 }
             }
         }
-    
-    private weak var fieldBackground: UIView!
-    private weak var textFieldConstraint: NSLayoutConstraint!
-    private weak var textField: UITextField!
-
-}
-
-extension ARTextCapture: UITextFieldDelegate {
-
-    // new name captured at this point
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return false
     }
 }
